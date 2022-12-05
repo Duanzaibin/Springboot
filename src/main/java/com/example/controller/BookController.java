649bce185503da1323180df653487b257625db86
Aurora <Aurora@DY.com>
2022-12-05 20:06:45 +0800

    Initial commit

---
+++ src/main/java/com/example/controller/BookController.java
@@ -0,0 +1,24 @@
+package com.example.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @GetMapping("/{id}")
+    public String getById(@PathVariable Integer id){
+        System.out.println("id ===>"+id);
+        return "hello spring boot!";
+    }
+
+    @GetMapping
+    public String getAll(){
+        System.out.println("spring boot start...");
+        return "spring boot start...";
+    }
+}
